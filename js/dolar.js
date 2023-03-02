340f963304398d7fd98266dac5283e822315da68
iCantarino <119424926+iCantarino@users.noreply.github.com>
2023-03-02 16:04:13 -0300

    Conversão Moeda v0.1

---
+++ js/dolar.js
@@ -0,0 +1,7 @@
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL').then(response =>  {
+    return response.json() 
+}).then(economia => {
+    console.log(economia)
+    document.getElementById("dolar").value = economia.USDBRL.bid;
+    document.getElementById("moeda").innerHTML = economia.USDBRL.code;
+})
